76af27e49e723d7dc2f475460482257b4e2c5053
Louise Reid <l_reid@hotmail.co.uk>
2019-03-12 15:43:34 +0000

    component views and routes

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Home from "@/views/Home.vue";
+import Cost from "@/views/Cost.vue";
+import Standard from "@/views/Standard.vue";
 
 Vue.use(Router);
 
@@ -12,13 +14,14 @@ export default new Router({
       component: Home
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/cost",
+      name: "cost",
+      component: Cost
+    },
+    {
+      path: "/standard",
+      name: "standard",
+      component: Standard
     }
   ]
 });
