c9195a79b3cb3b6212c722320af67ac5f14ab94e
Louise Reid <l_reid@hotmail.co.uk>
2019-03-13 15:57:04 +0000

    cards displaying with city name

--- src/views/Standard.vue
+++ src/views/Standard.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>Standard of Living</div>
+  <div>
+    <v-progress-circular v-if="cities.length === 0" indeterminate color="#829fce"/>
+    <v-container grid-list-md text-xs-center>
+      <v-layout row wrap>
+        <v-flex xs4 v-for="(city, index) in cities" :key="index" >
+          <v-card  class="primary">
+            <v-card-text primary-title>
+              <div>
+                <h3 class="mb-0 text-capitalize">{{city.name}}</h3>
+              </div>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -14,17 +29,19 @@ export default {
     EventService.getScores()
       .then(res =>
         res.map(city => ({
+        name: city.config.url.split('/').slice(-3)[0].replace('slug:', '').replace(/-/g, ' '),
           categories: city.data.categories,
           summary: city.data.summary.replace(/<\/?[^>]+(>|$)/g, "")
         }))
       )
       .then(cities => (this.cities = cities))
-      .then(() => {
-        this.cities.map(city => (city.city = city.summary.split(",")[0]));
-      });
   }
 };
 </script>
 
 <style scoped>
+
+h3 {
+    text-align: center!important
+}
 </style>
