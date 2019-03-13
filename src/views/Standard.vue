4f9d90559fca67c9f31d743a3b9d39a207b6fa03
Louise Reid <l_reid@hotmail.co.uk>
2019-03-13 16:50:04 +0000

    card display

--- src/views/Standard.vue
+++ src/views/Standard.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
     <v-progress-circular v-if="cities.length === 0" indeterminate color="#829fce"/>
-    <v-container grid-list-md text-xs-center>
+    <v-container grid-list-md>
       <v-layout row wrap>
-        <v-flex xs4 v-for="(city, index) in cities" :key="index" >
-          <v-card  class="primary">
-            <v-card-text primary-title>
-              <div>
-                <h3 class="mb-0 text-capitalize">{{city.name}}</h3>
-              </div>
+        <v-flex xs3 v-for="(city, index) in cities" :key="index">
+          <v-card class="ma-2">
+            <v-card-text primary-title class="pa-2">
+              <h3 class="text-capitalize text-sm-center">{{city.name}}</h3>
+            </v-card-text>
+            <v-card-text
+              v-for="(category, index) in city.categories"
+              :key="index"
+              class="pa-0 mb-0"
+            >
+              <p
+                class="mb-1 text-sm-center"
+                secondary-title
+              >{{category.name}}: {{category['score_out_of_10'].toFixed(2)}}</p>
+            </v-card-text>
+            <v-card-text class="font-weight-bold">
+              <p class="text-md-center">Teleport City Score: {{city.score.toFixed(2)}}</p>
             </v-card-text>
           </v-card>
         </v-flex>
@@ -29,19 +40,20 @@ export default {
     EventService.getScores()
       .then(res =>
         res.map(city => ({
-        name: city.config.url.split('/').slice(-3)[0].replace('slug:', '').replace(/-/g, ' '),
+          name: city.config.url
+            .split("/")
+            .slice(-3)[0]
+            .replace("slug:", "")
+            .replace(/-/g, " "),
           categories: city.data.categories,
-          summary: city.data.summary.replace(/<\/?[^>]+(>|$)/g, "")
+          score: city.data["teleport_city_score"]
         }))
       )
-      .then(cities => (this.cities = cities))
+      .then(cities => (this.cities = cities));
   }
 };
 </script>
 
 <style scoped>
 
-h3 {
-    text-align: center!important
-}
 </style>
